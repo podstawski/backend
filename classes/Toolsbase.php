604a48ba8b75a220b3e411f13a9df3f793f2a229
Piotr Podstawski <piotr@webkameleon.com>
2015-08-14 07:01:22 +0000

    ok

--- classes/Toolsbase.php
+++ classes/Toolsbase.php
@@ -278,4 +278,45 @@ class Toolsbase {
 	
 
     }
+	
+	
+	public static function logrotator()
+	{
+	
+		$base=Tools::saveRoot('log');
+		
+		$ts=time()-24*3600;
+		$year=date('Y',$ts);
+		$month=date('m',$ts);
+		$day=date('d',$ts);
+		
+		foreach (scandir($base) AS $component)
+		{
+			echo "Starting $component<br/>";
+			if ($component[0]=='.') continue;
+			if (substr($component,-1)=='/') $component=substr($component,0,strlen($component)-1);
+			$dir="$base/$component/$year/$month/$day";
+			
+			echo "&nbsp; Scaning dir $dir<br/>";
+			if (!file_exists($dir))
+			{
+				echo "&nbsp; &nbsp; does not exist<br/>";
+				continue;
+			}
+			
+			$log='';
+			foreach(scandir($dir) AS $f)
+			{
+				if ($f[0]=='.') continue;
+				$log.=file_get_contents("$dir/$f");
+				unlink("$dir/$f");
+			}
+			file_put_contents("$base/$component/$year/$month/$day.txt",$log);
+			@unlink($dir);
+			@rmdir($dir);
+		}
+	}
+
+	
+	
 }
